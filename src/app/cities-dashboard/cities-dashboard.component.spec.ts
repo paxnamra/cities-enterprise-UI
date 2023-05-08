6edd56a425b0e49f67aa60d22e70cfccdd30c92e
paxnamra <bialyfeniks4@gmail.com>
2023-05-08 02:18:30 +0200

    added dashboard component

---
+++ src/app/cities-dashboard/cities-dashboard.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CitiesDashboardComponent } from './cities-dashboard.component';
+
+describe('CitiesDashboardComponent', () => {
+  let component: CitiesDashboardComponent;
+  let fixture: ComponentFixture<CitiesDashboardComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ CitiesDashboardComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(CitiesDashboardComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/cities-dashboard/cities-dashboard.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cities-dashboard',
+  templateUrl: './cities-dashboard.component.html',
+  styleUrls: ['./cities-dashboard.component.css']
+})
+export class CitiesDashboardComponent {
+
+}
