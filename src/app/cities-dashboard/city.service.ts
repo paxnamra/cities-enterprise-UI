09653d66c769ee8d0bc4577501d4e24cb937ac27
paxnamra <paxnamra@gmail.com>
2023-07-18 00:40:08 +0200

    implemented logic for city service

--- src/app/cities-dashboard/city.service.ts
+++ src/app/cities-dashboard/city.service.ts
@@ -1,10 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
 
 @Injectable({
     providedIn: 'root'
 })
 export class CityService {
-    constructor() {
+    private CITIES_API = "http://localhost:8080/api/cities";
+
+    constructor(private http: HttpClient) {
     }
 
+    fetchCities(): Observable<any> {
+        return this.http.get<any>(this.CITIES_API);
+    }
 }
